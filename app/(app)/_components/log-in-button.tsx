56a4b6721edca0d7a92692adf07d73a2f08cd547
Victor Lawrence <viclawrence99@gmail.com>
2025-01-10 20:52:05 +0100

    please wait

--- app/(app)/_components/log-in-button.tsx
+++ app/(app)/_components/log-in-button.tsx
@@ -1,31 +1,40 @@
-'use client'
+"use client";
 
-import React from 'react'
+import React from "react";
 
-import { Button } from '@/components/ui'
+import { Button } from "@/components/ui";
 
-import { useLogin } from '@/hooks'
-import { Wallet } from '@privy-io/react-auth'
+import { useLogin } from "@/hooks";
+import { Wallet } from "@privy-io/react-auth";
 
 interface Props {
-    onComplete?: (wallet: Wallet) => void
+  onComplete?: (wallet: Wallet) => void;
 }
 
 const LogInButton: React.FC<Props> = ({ onComplete }) => {
+  const { login, user, connectWallet } = useLogin({
+    onComplete,
+  });
 
-    const { login, user, connectWallet } = useLogin({
-        onComplete
-    })
+  const { address, isConnected, caipAddress, status, embeddedWalletInfo } =
+    useAppKitAccount();
 
-    return (
-        <Button 
-            variant="brand"
-            onClick={() => { if(user) { connectWallet() } else { login() } }}
-            className="w-full"
-        >
-            Connect Wallet
-        </Button>
-    )
-}
+  const { isReady, isPending, connect } = useAppKitWallet();
+  return (
+    <Button
+      variant="brand"
+      onClick={() => {
+        if (user) {
+          connectWallet();
+        } else {
+          login();
+        }
+      }}
+      className="w-full"
+    >
+      Connect Wallet
+    </Button>
+  );
+};
 
-export default LogInButton
+export default LogInButton;
